22c6ecf117a6bb9aa0a685d41f119d93011c6a67
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-05-28 15:45:09 +0530

    Stage4Day8

---
+++ Stage4/Day8/KafkaConsumerApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace KafkaConsumerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset= AutoOffsetReset.Earliest,
+                GroupId ="g3-consumer-group"
+            };
+            using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            consumer.Subscribe("gayathrimsg_exchange");
+            var cancelToken = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cancelToken.Cancel();
+            };
+            try
+            {
+                while (true)
+                {
+                    var consumeMessage = consumer.Consume(cancelToken.Token);
+                    Console.WriteLine(consumeMessage.Value+"    "+consumeMessage.Topic);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                consumer.Close();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stage4/Day8/KafkaProducerApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace KafkaProducerApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+            int i = 1;
+            while (true)
+            {
+                var message = new Message<Null, string>
+                {
+                    Value = "G3 sending message from app " + i
+                };
+                i++;
+                var produceMessage = await producer.ProduceAsync("gayathrimsg_exchange", message);
+                Console.WriteLine("The message sent is "+produceMessage.Value+" in the topic "+produceMessage.Topic);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
